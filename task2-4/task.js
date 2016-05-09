6a59cd39263f3690aad3967ab46d273b1f34ee12
ruling <rulingfong@outlook.com>
2016-05-09 14:26:04 +0800

    完成任务4

--- task2-4/task.js
+++ task2-4/task.js
@@ -17,9 +17,16 @@ function addAqiData() {
 	aqiCityInput = document.getElementById('aqi-city-input').value;
  	aqiValueInput = document.getElementById('aqi-value-input').value;
  	if (aqiCityInput !== "" && aqiValueInput !== "") {
- 		aqiValueInput = aqiValueInput.replace(/[^\s]*|[\s]*$/g,'')
- 		aqiData[aqiCityInput] = aqiValueInput;
- 	}
+ 		aqiValueInput = aqiValueInput.replace(/(^\s*)|(\s*$)/g, '');
+ 		aqiCityInput = aqiCityInput.replace(/(^\s*)|(\s*$)/g, '');
+ 		if (/^([a-zA-Z\u4e00-\u9fa5]*)\s*([a-zA-Z\u4e00-\u9fa5]*$)/g.test(aqiCityInput) && /^\d+$/g.test(aqiValueInput)) {
+ 			var city = aqiCityInput;
+ 			var value = parseInt(aqiValueInput, 10);
+ 			if (!aqiData.hasOwnProperty(city)) {
+ 				aqiData[city] = value;
+ 			} else {alert("该城市已存在！")}
+ 		} else {alert("输入格式不正确！\n城市名仅接受中英文字符和空格符；\n污染指数仅接受数字")}
+ 	} else {alert("未输入或输入不完整")}
 	
 }
 
@@ -27,7 +34,17 @@ function addAqiData() {
  * 渲染aqi-table表格
  */
 function renderAqiList() {
-
+ var tab = document.getElementById('aqi-table');
+ tab.innerHTML = "";
+ var tr1 = document.createElement('tr');
+ tr1.innerHTML = "<td>城市</td><td>空气质量</td><td>操作</td>";
+ tab.appendChild(tr1);
+ var x;
+ for (x in aqiData) {
+ 	var tr2 = document.createElement('tr');
+ 	tr2.innerHTML = "<td>" + x + "</td><td>" + aqiData[x] + "</td><td><button>删除</button></td>";
+ 	tab.appendChild(tr2);
+ }
 }
 
 /**
@@ -45,7 +62,8 @@ function addBtnHandle() {
  */
 function delBtnHandle() {
   // do sth.
-
+  var city = event.target.parentNode.parentNode.children[0].textContent;
+  delete aqiData[city];
   renderAqiList();
 }
 
@@ -55,9 +73,10 @@ function init() {
   window.addEventListener('load',function(){
   	var addBtn = document.getElementById('add-btn');
   	addBtn.addEventListener('click', addBtnHandle);
-  })
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-
+  	var tab = document.getElementById('aqi-table');
+  	tab.addEventListener('click', delBtnHandle);
+  })
 }
 
 init();
